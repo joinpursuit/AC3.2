8c2b4b64d6a929e9979cd93c2bbef6a0077c88ce
Madushani Lekam Wasam Liyanage <Shashi@Shashis-MacBook.local>
2016-08-25 15:11:23 -0400

    Exercises 8/25

--- lessons/arrays/exercises/8-24-array-exercises.playground/Contents.swift
+++ lessons/arrays/exercises/8-24-array-exercises.playground/Contents.swift
@@ -234,6 +234,7 @@ print(sumOfDiagonals)
 
 var toRotate = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
+
 //16)
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23
 //Find the sum of all the multiples of 3 or 5 below 1000
