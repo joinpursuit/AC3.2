f9e6c4ffb9f4c736815ab8e880e77ee8deee1af5
Miti Shah <mitishah@C4Qs-MacBook-Pro-6.local>
2016-08-25 15:21:52 -0400

    8-25 exercises

--- lessons/dictionaries/8-25 exercises.playground/Contents.swift
+++ lessons/dictionaries/8-25 exercises.playground/Contents.swift
@@ -2,6 +2,186 @@
 
 import UIKit
 
+//1)
+
+//a) Create an instance of a dictionary called citiesDict that maps 3 countries to their capital cities
+
+
+var citiesDict: [String : String] = ["India" : "New Delhi", "US" : "Washington, DC", "England" : "London"]
+
+
+//b) Add two more countries to your dictionary
+
+
+citiesDict["France"] = "Paris"
+citiesDict["Germany"] = "Berlin"
+
+
+//c) Translate at least 3 of the capital names into another language
+
+
+citiesDict.updateValue("Parijs", forKey: "France")
+citiesDict.updateValue("Berlijn", forKey: "Germany")
+citiesDict.updateValue("Londen", forKey: "England")
+
+
+//2)
+
+var someDict:[String:Int] = ["One": 1, "Two": 4, "Three": 9, "Four": 16, "Five": 25]
+
+//a) using someDict, add together the values associated with "Three" and "Five" and print the result.
+
+
+print(someDict["Three"]! + someDict["Five"]!, terminator: "")
+print("") //formatting purposes
+
+
+//b) Add values to the dictionary for the keys "Six" and "Seven"
+
+
+someDict["Six"] = 36
+someDict["Seven"] = 49
+
+
+//c) Make a key caled "productUpToSeven" and set its value equal to the product of all the values
+
+
+var sum = 0
+
+for (key, value) in someDict {
+    sum += value
+}
+someDict["productUpToSeven"] = sum
+
+
+//d) Make a key called "sumUpToSix" and set its value equal to the sum of the keys "One", "Two", "Three", "Four", "Five" and "Six"
+
+
+var sumOfKeys = 0
+
+for (key, value) in someDict {
+    if key == "Seven" || key == "productUpToSeven" {
+        continue
+    } else {
+        sumOfKeys += value
+    }
+}
+someDict["sumUpToSix"] = sumOfKeys
+
+
+//e) Remove the new keys made for parts c and d
+
+
+someDict.removeValueForKey("productUpToSeven")
+someDict.removeValueForKey("sumUpToSix")
+
+
+//f) Add 2 to every value inside of someDict
+
+
+for (key, value) in someDict {
+    var sum = value + 2
+    someDict[key] = sum
+}
+
+
+//3)  (from http://www.themobilemontage.com/wp-content/uploads/2015/05/hw1.pdf)
+//a) Create a variable that is explicitly typed as a dictionary that maps strings to floating point numbers. Initialize the variable to the data shown in the table below which lists an author name and their comprehensibility score.
+/*
+ “Mark Twain” - 8.9
+ “Nathaniel Hawthorne” - 5.1
+ “John Steinbeck” - 2.3
+ “C.S. Lewis” - 9.9
+ “Jon Krakaur” - 6.1
+ */
+
+
+var bookDict: [String : Float] = ["Mark Twain" : 8.9, "Nathaniel Hawthorne" : 5.1, "John Steinbeck" : 2.3, "C.S. Lewis" : 9.9, "Jon Krakaur" : 6.1]
+
+
+//b) Using the dictionary created in the previous problem, do the following: Print out the floating-point score for “John Steinbeck”. Add an additional author named “Erik Larson” with an assigned score of 9.2.  Write an if/else statement that compares the score of John Krakaur with Mark  Twain. Print out the name of the author with the highest score.
+
+
+print(bookDict["John Steinbeck"]!, terminator: "")
+print("") //formatting purposes
+bookDict["Erik Larson"] = 9.2
+
+if bookDict["John Krakaur"] > bookDict["Mark Twain"] {
+    print("John Krakaur has a higher comprehensibility score than Mark Twain.")
+} else {
+    print("Mark Twain has a higher comprehensibility score than John Krakaur")
+}
+
+
+//c)  Use a for loop to iterate through the dictionary created in problem 3a and print out the content in the form of key: value, one entry per line.
+
+
+for (key, value) in bookDict {
+    print("\(key): \(value)")
+}
+
+
+//4 -7 source :  https://www.weheartswift.com/dictionaries/)
+//4)
+//4a)You are given a dictionary code of type [String:String] which has values for all lowercase letters. The code dictionary represents a way to encode a message. For example if code["a"] = "z" and code["b"] = "x" the encoded version if "ababa" will be "zxzxz". You are also given a message which contains only lowercase letters and spaces. Use the code dictionary to encode the message and print it.
+
+var code = [
+    "a" : "b",
+    "b" : "c",
+    "c" : "d",
+    "d" : "e",
+    "e" : "f",
+    "f" : "g",
+    "g" : "h",
+    "h" : "i",
+    "i" : "j",
+    "j" : "k",
+    "k" : "l",
+    "l" : "m",
+    "m" : "n",
+    "n" : "o",
+    "o" : "p",
+    "p" : "q",
+    "q" : "r",
+    "r" : "s",
+    "s" : "t",
+    "t" : "u",
+    "u" : "v",
+    "v" : "w",
+    "w" : "x",
+    "x" : "y",
+    "y" : "z",
+    "z" : "a"
+]
+
+var message = "hello world"
+
+
+if let h = code["g"], e = code["d"], l = code["k"], o = code["n"], w = code["v"], r = code["q"], d = code["c"] {
+    print("\(h)\(e)\(l)\(l)\(o)", "\(w)\(o)\(r)\(l)\(d)", terminator: "")
+}
+print("") //formatting purposes
+
+
+//4b)
+//You are also given a encodedMessage which contains only lowercase letters and spaces. Use the code dictionary to decode the message and print it.
+
+var encodedMessage = "uijt nfttbhf jt ibse up sfbe"
+
+
+for i in encodedMessage.characters {
+    if String(i) == " " {
+        print(" ", separator: "", terminator: "")
+    }
+    for (k, v) in code {
+        if String(i) == v {
+            print(k, terminator: "")
+        }
+    }
+}
+
+////////  Only got this far
+
 
 //1)
 
@@ -10,6 +190,11 @@ import UIKit
 //b) Add two more countries to your dictionary
 
 //c) Translate at least 3 of the capital names into another language
+
+
+
+
+
 
 //2)
 
