f54eb66695851adc7a1978673ae67055b600f57f
Karen Manzanares Fuentes <Karen@Karens-MacBook-Pro-2.local>
2016-08-30 17:57:52 -0400

    exercise 8-30

--- lessons/functions-two/exercises/functions-two-exercises.playground/Contents.swift
+++ lessons/functions-two/exercises/functions-two-exercises.playground/Contents.swift
@@ -3,32 +3,68 @@
 import UIKit
 
 //1a) Write a function so that it will print out total cost after tip.
-
-let itemCost = 45
-let tipPercentage = 0.15
+//
+//let itemCost = 45
+//let tipPercentage = 0.15
 
 //Write your code below
 
+//func totalWithTip (itemCost x: Double, tipPercentage y: Double) ->Double{
+//         return (x*y) + x
+//
+//}
+//
+//let myFinalCost = totalWithTip(itemCost: 45 , tipPercentage: 0.15) //Fill in the arguments
 
+//print(myFinalCost)
 
-let myFinalCost = totalWithTip() //Fill in the arguments
 
 //1b)Write a function  that it will print out total cost after tip and tax
-let taxPercentage = 0.09
+//let taxPercentage = 0.09
+//
+////Write your code below
+//
+//func totalWithTipAndTax (itemCost x:Double, tipPercentage y: Double, taxPercentage z:Double) -> Double {
+//    return (x*y) + (x*z) + x
+//}
+//
+//let myFinalCostWithTipAndTax = totalWithTipAndTax(itemCost: 45, tipPercentage: 0.15, taxPercentage: 0.09) //Fill in the arguments
+//
+//print(myFinalCostWithTipAndTax)
 
-//Write your code below
 
-
-let myFinalCostWithTipAndTax = totalWithTipAndTax() //Fill in the arguments
 
 
 //2a)Write a function takes an Int as input, and returns true if it is even, and false if it is odd
 
 //Write your code below
 
+//func evenOrOdd (numb:Int) -> Bool {
+//    if (numb%2 == 0) {
+//        return true
+//    } else {
+//        return false
+//    }
+//}
+//
+//print (evenOrOdd(18))
+
+
 //2b) Using function above, write code that prints out whether dieRoll is even or odd
 
-let dieRoll = Int(arc4random_uniform(6) + 1)
+//func dieRollEvenOrOdd () -> Bool{
+//    let dieRoll = Int(arc4random_uniform(6) + 1)
+//    if (dieRoll%2==0) {
+//        return true
+//    }else {
+//        return false
+//    }
+//
+//}
+//
+//print(dieRollEvenOrOdd())
+
+
 
 //3)
 /*
@@ -40,6 +76,23 @@ let dieRoll = Int(arc4random_uniform(6) + 1)
  Your function should take in one parameter: x (the number to count up to)
  */
 
+//func fizzBuzz(x:Int) {
+//    for i in 1..<x {
+//        if (i%3 == 0 && i%5==0){
+//            print( "fizz buzz")
+//        } else if (i%3==0){
+//            print("fizz")
+//        } else if (i%5==0){
+//            print ("buzz")
+//        } else {
+//            print(i)
+//        }
+//    }
+//
+//}
+//
+//fizzBuzz(20)
+//    
 
 
 
@@ -47,37 +100,132 @@ let dieRoll = Int(arc4random_uniform(6) + 1)
 
 //Write your code below
 
+//func largest( numb:[Int]) -> Int {
+//var large = 0
+//for number in numb{
+//    if number > large {
+//         large = number
+//        
+//        }
+//    }
+//    return large
+//}
+//
+//
+//print(largest([1,4,8,10,55,79]))
+
+
+
 
 
 //4b)
-let myArray = [3,5,1,3,532,1,4,91,20,30,92,143]
+//let myArray = [3,5,1,3,532,1,4,91,20,30,92,143]
 
 //Using your function in part a, use String interpolation to print a sentence that states what the largest Int in myArray is
 
 
+
+
+//let myArray = [3,5,1,3,532,1,4,91,20,30,92,143]
+//
+//func largest( numb:[Int]) -> String {
+//var large = 0
+//for number in numb{
+//    if number > large {
+//         large = number
+//
+//        }
+//    }
+//    return ("\(large) is the largest Int in myArray")
+//}
+//
+//print(largest(myArray))
+
+
+
+
 //4c)
 //Using your solution to 2a), print a sentence that states whether the largest Int in myArray is even or odd
+
+//let myArray = [3,5,1,3,532,1,4,91,20,30,92,143]
+//
+//func largest( numb:[Int]) -> String {
+//var large = 0
+//for number in numb{
+//    if number > large {
+//         large = number
+//
+//        }
+//    }
+//    
+//let x = large
+//    if (x%2==0){
+//        return ("\(x) is even and is the largest number in my array")
+//    } else {
+//        return ("\(x) is odd and is the largest number in my array")
+//    }
+//}
+//
+//print(largest(myArray))
+
+
 
 
 //5a) Write a function that takes a String as input and returns the number of characters in the string
 
 //Write your code below
 
+//func mystringone (mystring : String) -> Int{
+//var myarray: [Character] = []
+//for i in mystring.characters{
+//    myarray.append(i)
+//    }
+//    
+//return (myarray.count)
+//}
+//mystringone("hello bob")
+
+
 
 
 //5b) Using your function above, print how many characters are in myString
 
-let myString = "Swift is a new programming language for iOS, OS X, watchOS, and tvOS apps that builds on the best of C and Objective-C, without the constraints of C compatibility."
+//let myString = "Swift is a new programming language for iOS, OS X, watchOS, and tvOS apps that builds on the best of C and Objective-C, without the constraints of C compatibility."
+//
+//func mystringone (string : String) -> Int{
+//var myarray: [Character] = []
+//for i in string.characters{
+//    myarray.append(i)
+//    }
+//
+//return (myarray.count)
+//}
+//print(mystringone(myString))
+//
 
 
 //5c) Write a function that counts how many characters in a String match a specific character.  (e.g: count how many "a"s are in a String, or count how many ","s are in a String.
 
 //Sample input:
-//let FiveCString = "This is a test string for your code"
+let FiveCString = "This is a test string for your code"
 //let targetCharacter = "i"
 
 //Sample output:
 //3
+func mystring3(mystring: String, myChar: Character)-> Int{
+//    var counter = 0
+    var myarraytwo:[Character] = [ ]
+    for i in mystring.characters{
+        if i == myChar{
+            counter += 1
+        }
+        
+        
+       
+    }
+}
+
+
 
 
 
