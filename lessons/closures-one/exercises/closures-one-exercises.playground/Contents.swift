3fb798d8154e3d6e8235ce42cb0240faeea84604
Annie Tung <annietung@C4Qs-MacBook-Pro-34.local>
2016-08-31 18:02:04 -0400

    8-31-exercise

--- lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
+++ lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
@@ -6,15 +6,72 @@ import UIKit
 
 //1. Create a closure that has no parameters or values and prints "Hello Closures!". Check by passing closure's return to a variable
 
+var myClosure = {() -> String in
+    return "Hello Closures!"
+}
+myClosure()
+
 //2. Create a closure that takes one Int and returns the doubled value. Check by passing closure's return to a variable.
+
+var myClosureTwo = {(Int) -> Double in
+    return Int * 2
+}
+myClosureTwo(5)
 
 //3. Create a closure that takes one Int and returns a bool whether or not it's divisible by 3.
 
+var myClosureThree = {(Int) -> Bool in
+    return Int % 3 == 0
+}
+myClosureThree(6)
+
 //4. Create a closure that takes two strings as input and returns the longest character count of the two strings.
 
+var myClosureFour = {(a: String, b: String) -> Int in
+    if a.characters.count > b.characters.count {
+        return a.characters.count
+    }
+    else {
+        return b.characters.count
+    }
+}
+myClosureFour("Hello", "World!")
+
 //5a. Create a closure that takes an array of Int as input and returns the largest element in the array
+var array = [1,23,384,2]
+
+var myClosureFive = {(c: [Int]) -> Int in
+    var max = c[0]
+    for i in 0..<c.count {
+        if c[i] > max {
+            max = c[i]
+        }
+    }
+    return max
+}
+print(myClosureFive(array))
 
 //5b.  Create a closure that takes an array of Int and variable x: Int as input and returns the xth largest element in the array.  Assume x is always < the count of the array
+var arrayTwo = [1,42,35,67,6,3,4]
+
+var myClosureSix = {(newArray: [Int], x: Int) -> Int? in
+    
+    if newArray.count < x - 1 { return nil }
+    
+    var largestEle: Int?
+    
+    let newerArray = newArray.sort(>)
+    
+    for i in 0..<newerArray.count {
+        if i == x - 1 {
+            largestEle = newerArray[i]
+            return largestEle
+        }
+    }
+    return nil
+}
+
+myClosureSix(arrayTwo, 9)
 
 //5c.  Rewrite 5b and add handling for cases where x >= the count of the array (Hint: Use optionals)
 
@@ -22,21 +79,55 @@ import UIKit
 
 let myArray = [34,42,42,1,3,4,3,2,49]
 
+var myClosureSeven = {(array: [Int], x1: Int) -> Int? in
+    if x1 >= array.count {
+        return nil
+    }
+    else {
+        return x1
+    }
+}
+myClosureSeven(myArray, 10)
+
 //6a. Sort myArray in ascending order by defining the constant ascendingOrder below.
 
+let ascendingOrder = myArray.sort {$0 < $1}
 //let mySortedArray = myArray.sort(ascendingOrder)
-//let ascendingOrder =
 
 //6b. Sort myArray in descending order by defining the constant descendingOrder below.
 
+let descendingOrder = myArray.sort {$0 > $1}
 //let mySecondSortedArray = myArray.sort(descendingOrder)
-//let descendingOrder =
+
 
 let arrayOfArrays = [[3,65,2,4],[25,3,1,6],[245,2,3,5,74]]
 
 //7a. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Assume each array will have at least 3 elements
-
+var newArrayOfArrays = arrayOfArrays.sort{(a: [Int], b: [Int]) -> Bool in
+    var arrOne = a[2]
+    var arrTwo = b[2]
+    return arrOne < arrTwo
+}
+print(newArrayOfArrays)
+///////
 //7b. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Don't assume each array will have at least 3 elements.  Put all arrays that have less than 3 elements at the end in any order.
+
+var newArray = {(c: [[Int]], elementNum: Int) -> [[Int]] in
+    var sortedArr = [[Int]]()
+    var unsortedArr = [[Int]]()
+    for i in c {
+        if elementNum <= c.count {
+            sortedArr.append(i)
+        }
+        else {
+            unsortedArr.append(i)
+        }
+    }
+    return sortedArr.sort({$0[elementNum-1] < $1[elementNum-1]}) + unsortedArr
+}
+
+//print(newArray(arrayOfArrays 3, 4))
+///////
 
 let letterValues = [
     "a" : 54,
@@ -66,20 +157,37 @@ let letterValues = [
     "y" : 123,
     "z" : 432
 ]
-
+//////////
 //8a. Sort the string below in descending order according the dictionary letterValues
 
 var codeString = "aldfjaekwjnfaekjnf"
 
-
+//making it an array first
+let myString = Array(codeString.characters)
+myString.sort {(a: Character, b: Character) -> Bool in
+    //takes closure as argument takes a and b returning the value associated with key a and b.
+    let aValue = letterValues[String(a)] //character needs to be converted to a string
+    let bValue = letterValues[String(b)]
+    return aValue < bValue
+}
+let myInt = 25342
+let myIntAsString = String(myInt)
+print(myIntAsString)
+///////////
 //8b.  Sort the string below in ascending order according the dictionary letterValues
 
 var codeStringTwo = "znwemnrfewpiqn"
 
+//var emptyString = ""
+//for i in codeString.characters.sort({a, b in letterValues[String(a)] > letterValues[String(b)]}) {
+//    emptyString.append(i)
+//}
+//print(emptyString)
 
+///////
 
 //9.  Write a function that takes a function as input and returns a function that doubles the output of the input function
-
+//
 //Input: (Int) -> Int
 //Output: (Int) -> Int
 
@@ -91,7 +199,7 @@ var number = 1
 
 
 
-//9. Given a tuple representation of our names from before:
+//11. Given a tuple representation of our names from before:
 
 let firstAndLastTuples = [("Johann S.", "Bach"),
                                  ("Claudio", "Monteverdi"),
@@ -109,7 +217,7 @@ let firstAndLastTuples = [("Johann S.", "Bach"),
 // .
 // .
 
-//10. Build an array of tuples representing everyone in the class. Here you are sorted by first name:
+//12. Build an array of tuples representing everyone in the class. Here you are sorted by first name:
 //
 
 //Amber Spadafora	3201
@@ -161,7 +269,7 @@ let ac32folks = [("Amber", "Spadafora",	3201),
 
 
 
-//11. Create a closure that takes an two arrays of strings as input. Output a new string with the contents of the arrays in alternating order and separated by a space. If one array's length is longer than the other, append the rest of it's contents to the new string.
+//13. Create a closure that takes an two arrays of strings as input. Output a new string with the contents of the arrays in alternating order and separated by a space. If one array's length is longer than the other, append the rest of it's contents to the new string.
 
 // eg: input array1: ["Hello", "My", "Friend"] array2: ["Darkness", "Old"]
 //      output string: "Hello Darkness My Old Friend
--- lessons/functions-two/exercises/functions-two-exercises.playground/Contents.swift
+++ lessons/functions-two/exercises/functions-two-exercises.playground/Contents.swift
@@ -4,31 +4,49 @@ import UIKit
 
 //1a) Write a function so that it will print out total cost after tip.
 
-let itemCost = 45
-let tipPercentage = 0.15
+let ic = 45.0
+let tp = 0.15
 
 //Write your code below
+func totalWithTip(priceOfItem itemCost: Double, tipCost tipPercentage: Double) -> Double {
+    return itemCost + tipPercentage
+}
+print(totalWithTip(priceOfItem: Double(ic), tipCost: tp))
 
-
-
-let myFinalCost = totalWithTip() //Fill in the arguments
+let myFinalCost = totalWithTip(priceOfItem: Double(ic), tipCost: tp) //Fill in the arguments
 
 //1b)Write a function  that it will print out total cost after tip and tax
 let taxPercentage = 0.09
 
 //Write your code below
+func totalWithTipAndTax(priceOfItem itemCost: Double, tipCost tipPercentage: Double, taxCost taxPercentage: Double) -> Double {
+    return itemCost * (taxPercentage + 1) + tipPercentage
+}
+print(totalWithTipAndTax(priceOfItem: Double(ic), tipCost: tp, taxCost: taxPercentage))
 
-
-let myFinalCostWithTipAndTax = totalWithTipAndTax() //Fill in the arguments
-
+let myFinalCostWithTipAndTax = totalWithTipAndTax(priceOfItem: Double(ic), tipCost: tp, taxCost: taxPercentage) //Fill in the arguments
 
 //2a)Write a function takes an Int as input, and returns true if it is even, and false if it is odd
 
 //Write your code below
+func EvenOdd(number: Int) -> Bool {
+    if number % 2 == 0 {
+        return true
+    }else{
+        return false
+    }
+}
+EvenOdd(2)
 
 //2b) Using function above, write code that prints out whether dieRoll is even or odd
 
 let dieRoll = Int(arc4random_uniform(6) + 1)
+if EvenOdd(dieRoll) {
+    print("even")
+}
+else {
+    print("odd")
+}
 
 //3)
 /*
@@ -39,64 +57,118 @@ let dieRoll = Int(arc4random_uniform(6) + 1)
  
  Your function should take in one parameter: x (the number to count up to)
  */
-
-
-
+func fb(x: Int) {
+    for num in 1...x {
+        if num % 3 == 0 && num % 5 == 0 {
+            print("FizzBuzz")
+        }else if num % 3 == 0 {
+            print("Fizz")
+        } else if num % 5 == 0 {
+            print("Buzz")
+        } else {
+            print(num)
+        }
+    }
+}
 
 //4a) Write a function that takes [Int] as input.  It should return the largest Int in the array.
 
 //Write your code below
-
-
+func array(numbers: [Int]) -> Int {
+    var largestInt = numbers[0]
+    for num in numbers {
+        if num > largestInt {
+            largestInt = num
+        }
+    }
+    return largestInt
+}
 
 //4b)
 let myArray = [3,5,1,3,532,1,4,91,20,30,92,143]
 
 //Using your function in part a, use String interpolation to print a sentence that states what the largest Int in myArray is
 
+array(myArray)
+var largestIntInMyArray = array(myArray)
+print("The largest Int in myArray is \(largestIntInMyArray)")
 
 //4c)
 //Using your solution to 2a), print a sentence that states whether the largest Int in myArray is even or odd
-
+if array(myArray) % 2 == 0 {
+    print("The largest Int is even")
+}
+else {
+    print("The largest Int in odd")
+}
 
 //5a) Write a function that takes a String as input and returns the number of characters in the string
 
 //Write your code below
-
+func myStringOne(i: String) -> Int {
+    return i.characters.count
+}
 
 
 //5b) Using your function above, print how many characters are in myString
 
 let myString = "Swift is a new programming language for iOS, OS X, watchOS, and tvOS apps that builds on the best of C and Objective-C, without the constraints of C compatibility."
-
+myStringOne(myString)
 
 //5c) Write a function that counts how many characters in a String match a specific character.  (e.g: count how many "a"s are in a String, or count how many ","s are in a String.
 
 //Sample input:
-//let FiveCString = "This is a test string for your code"
-//let targetCharacter = "i"
+let fiveCString = "This is a test string for your code"
+let targetCharacter: Character = "i"
 
 //Sample output:
 //3
 
+//func howManyCharacterMatch(char: Character, with thisString: String) {
+//    
+//}
+//
+//howManyCharacterMatch(targetCharacter, with: fiveCString)
 
+func howManyCharacters(sentence: String, compareChar: Character) -> Int {
+    var i = 0
+    for char in sentence.characters {
+        if char == compareChar {
+            i += 1
+        }
+    }
+    return i
+}
+howManyCharacters(fiveCString, compareChar: targetCharacter)
 
 //5d) Write a function that counts how many characters in a String match one of several possible characters.  (e.g: count how many vowels are in a String, or count how many "a"s "b"s and "c"s are in a Sting)
 
 //Sample input:
-//let FiveDString = "This one is a little more complicated"
-//let targetCharacters = ["a", "e", "i", "o" "u"]
+let fiveDString = "This one is a little more complicated"
+let targetCharacters = ["a", "e", "i", "o", "u"]
 
 //Sample output:
 //13
-
-
 //Write your code below
+func howManyCharMatch(st: String, ch: Character) -> Int {
+    var j = 0
+    for char in st.characters {
+        if char == "a" || char == "e" || char == "i" || char == "o" || char == "u" {
+            j += 1
+        }
+    }
+    return j
+}
+howManyCharMatch(fiveDString, ch: targetCharacter)
 
 
 //6) Write a function that returns the number of unique Ints in an array of Ints
 //Sample input:
-//let inputArray = [3,1,4,1,3,2,6,1,9]
+let inputArray = [3,1,4,1,3,2,6,1,9]
+
+[4,4,5,6]
+
+[4,5,6]
 
 //Sample output:
 //4
@@ -104,14 +176,58 @@ let myString = "Swift is a new programming language for iOS, OS X, watchOS, and 
 //Explanation:
 //2, 4, 6, 9 are unique in the array.  Every other number is not unique.
 
+func arrayOfInt(array: [Int]) -> [Int] {
+    let uniqueSet = Set(array)
+    let uniqueArray = Array(uniqueSet)
+    return uniqueArray
+}
+
+//func uniqueArray(array: [Int]) -> [Int] {
+//    var uniqueArray = [Int]()
+//    for num in array {
+//        for uniqueNum in uniqueArray {
+//            if num != uniqueNum {
+//                uniqueArray.append(num)
+//            } else {
+//                continue
+//            }
+//        }
+//    }
+//}
+
+arrayOfInt(inputArray)
 
 //7) Write a function that converts a binary number into decimal.  The binary number will be given as an array of Ints.
 
 //Sample input:
-//let binaryArray = [1,0,1,1,1,0,1]
+let binaryArray = [1,0,1,1,1,0,1]
 
 //Sample output:
 //93
+
+func convertIntToString(nums: [Int]) -> String {
+    var resultString = ""
+    for num in nums {
+        resultString += "\(num)"
+    }
+    return resultString
+}
+convertIntToString(binaryArray)
+
+func binaryNum(b: [Int]) -> Double {
+    let stringBinary = convertIntToString(b)
+    let num = Int(stringBinary, radix: 2)!
+//    guard let num = Int(stringBinary, radix: 2) else { return nil }
+    
+    return Double(num)
+}
+print(binaryNum(binaryArray))
+
+//if let binaryNum = binaryNum(binaryArray) {
+//    print(binaryNum)
+//}
+
+
 
 //8) Copy the movie dictionary from homework two and add another [String] to each movie.
 //   Name it "locations" and add some dummy data. Use the re-factored nested function
