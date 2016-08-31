bdbc2386754930d9ff7762a62990aa4bbf2d4e99
Tom Seymour <tomseymour@Toms-MacBook-Pro.local>
2016-08-31 19:04:17 -0400

    exercises 8/31 COMPLETE

--- lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
+++ lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
@@ -6,15 +6,59 @@ import UIKit
 
 //1. Create a closure that has no parameters or values and prints "Hello Closures!". Check by passing closure's return to a variable
 
+var variable1 = { () -> () in
+    print("Hello Closures")
+}
+variable1()
+
 //2. Create a closure that takes one Int and returns the doubled value. Check by passing closure's return to a variable.
+
+var variable2 = { (a: Int) -> Int in
+    return a * 2
+}
+print(variable2(24))
 
 //3. Create a closure that takes one Int and returns a bool whether or not it's divisible by 3.
 
+var isDivBy3 = { (a: Int) -> Bool in
+    return a % 3 == 0
+}
+
+
 //4. Create a closure that takes two strings as input and returns the longest character count of the two strings.
 
+var longestStringCount = { (a: String, b: String) -> Int in
+    if a.characters.count > b.characters.count {
+        return a.characters.count
+    } else {
+        return b.characters.count
+    }
+}
+longestStringCount("hello", "Have a nice day!")
+
 //5a. Create a closure that takes an array of Int as input and returns the largest element in the array
+var array5A = [1, 23, 45, 22, 78, 98, 56, 2, 7, 9, -102, -1]
+
+var largestEle5A = { (theArray: [Int]) -> Int in
+        var largest = theArray[0]
+    for num in theArray {
+        if num > largest {
+            largest = num
+        }
+    }
+    return largest
+}
+largestEle5A(array5A)
 
 //5b.  Create a closure that takes an array of Int and variable x: Int as input and returns the xth largest element in the array.  Assume x is always < the count of the array
+
+
+
+var xthElement = { (arrOfInt: [Int], x: Int) -> Int in
+    var sortedArr = arrOfInt.sort(>)
+    return sortedArr[x - 1]
+}
+xthElement(array5A, 2)
 
 //5c.  Rewrite 5b and add handling for cases where x >= the count of the array (Hint: Use optionals)
 
@@ -22,21 +66,73 @@ import UIKit
 
 let myArray = [34,42,42,1,3,4,3,2,49]
 
-//6a. Sort myArray in ascending order by defining the constant ascendingOrder below.
+var xthElementC = { (arrOfInt: [Int], x: Int) -> Int? in
+    if x > arrOfInt.count {
+        return nil
+    } else {
+        var sortedArr = arrOfInt.sort(>)
+        return sortedArr[x - 1]
+    }
+}
+xthElementC(myArray, 10)
 
-//let mySortedArray = myArray.sort(ascendingOrder)
-//let ascendingOrder =
 
-//6b. Sort myArray in descending order by defining the constant descendingOrder below.
+////6a. Sort myArray in ascending order by defining the constant ascendingOrder below.
 
-//let mySecondSortedArray = myArray.sort(descendingOrder)
-//let descendingOrder =
+let mySortedArray = myArray.sort(<)
+let ascendingOrder = myArray.sort({ (a: Int, b: Int) -> Bool in
+    return a < b
+})
+
+////6b. Sort myArray in descending order by defining the constant descendingOrder below.
+
+let mySecondSortedArray = myArray.sort(>)
+let descendingOrder = myArray.sort({ (a: Int, b: Int) -> Bool in
+    return a > b
+})
 
 let arrayOfArrays = [[3,65,2,4],[25,3,1,6],[245,2,3,5,74]]
 
-//7a. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Assume each array will have at least 3 elements
+////7a. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Assume each array will have at least 3 elements
+
+let sortByThird = arrayOfArrays.sort({ (a: [Int], b: [Int]) -> Bool in
+    return a[2] < b[2]
+})
+
+func sortByXth(arrOfArr: [[Int]], x: Int) -> [[Int]] {
+    
+    return arrayOfArrays.sort({ (a: [Int], b: [Int]) -> Bool in
+        return a[x - 1] < b[ x - 1]
+    })
+
+}
+print(sortByXth(arrayOfArrays, x: 2))
+
 
 //7b. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Don't assume each array will have at least 3 elements.  Put all arrays that have less than 3 elements at the end in any order.
+
+let arrayOfArrays2 = [[3,65,2,4],[25,3,1,6],[245,2,3,5,74], [87, 54], [7], [76, 4, 63, 74, 71], [65, 21]]
+
+func sortByXth7b(arrOfArr: [[Int]], x: Int) -> [[Int]] {
+    var validArr: [[Int]] = []
+    var invalidArr: [[Int]] = []
+    for arr in arrOfArr {
+        if x <= arr.count {
+            validArr.append(arr)
+        } else {
+            invalidArr.append(arr)
+        }
+    }
+    var validArrSort = validArr.sort({ (a: [Int], b: [Int]) -> Bool in
+        return a[x - 1] < b[ x - 1]
+    })
+    validArrSort += invalidArr
+    return validArrSort
+
+}
+print(sortByXth7b(arrayOfArrays2, x: 3))
+
+
 
 let letterValues = [
     "a" : 54,
@@ -71,11 +167,22 @@ let letterValues = [
 
 var codeString = "aldfjaekwjnfaekjnf"
 
+print(codeString.characters.sort{(a, b) -> Bool in
+    var valA = letterValues[String(a)]
+    var valB = letterValues[String(b)]
+    return valA < valB
+})
+
 
 //8b.  Sort the string below in ascending order according the dictionary letterValues
 
 var codeStringTwo = "znwemnrfewpiqn"
 
+print(codeStringTwo.characters.sort{(a, b) -> Bool in
+    var valA = letterValues[String(a)]
+    var valB = letterValues[String(b)]
+    return valA > valB
+})
 
 
 //9.  Write a function that takes a function as input and returns a function that doubles the output of the input function
@@ -83,11 +190,45 @@ var codeStringTwo = "znwemnrfewpiqn"
 //Input: (Int) -> Int
 //Output: (Int) -> Int
 
+//func addOneTo(x: Int) -> Int {
+//    return x + 1
+//}
+//addOneTo(5)
+//
+//func doubleThis(x: (Int)) -> ((Int) -> Int) {
+//    
+//     { (a: Int) -> Int in
+//        return
+//}
+//
+//doubleThis(addOneTo(5))
+
+//func funcTwo() -> Int {
+//    return 2
+//}
+//
+//func funcThree() -> Int {
+//    return 3
+//}
+
+
 //10.  Write a closure tripleNumber that takes no arguments and returns void.  It should multiply the global variable number by 3 each time the closure is run.
 
 var number = 1
 
-//var tripleNumber =
+var tripleNumber = {() -> () in
+    number *= 3
+}
+
+tripleNumber()
+tripleNumber()
+
+number
+
+tripleNumber()
+tripleNumber()
+
+number
 
 
 
@@ -109,6 +250,15 @@ let firstAndLastTuples = [("Johann S.", "Bach"),
 // .
 // .
 
+var sortedByLastName = firstAndLastTuples.sort{ (a, b) -> Bool in
+    return a.1 < b.1
+}
+print(sortedByLastName)
+for tup in sortedByLastName {
+    print("\(tup.1), \(tup.0)")
+}
+
+    
 //10. Build an array of tuples representing everyone in the class. Here you are sorted by first name:
 //
 
@@ -152,16 +302,63 @@ let firstAndLastTuples = [("Johann S.", "Bach"),
 // Here's an example of how to start:
 let ac32folks = [("Amber", "Spadafora",	3201),
                  ("Ana", "Ma",	3202),
-                 ("Annie", "Tung",	3203)]
+                 ("Annie", "Tung",	3203),
+                 ("Cristopher", "Chavez", 3204),
+                 ("Eashir", "Arafat", 3205),
+                 ("Edward", "Anchundia", 3206),
+                 ("Emily", "Chu", 3207),
+                 ("Eric", "Chang", 3208),
+                 ("Erica", "Stevens", 3209),
+                 ("Tom", "Seymour", 3233),
+                 ("Tong", "Lin", 3234)]
+
+
 // and so on...
 
 // Build a sort comparison closure that will bring your name as close to the top as possible.
 // We will use this to determine the order we use to access the microwave.
 // Feel free to add fields to the tuple to accomplish this -- yes, this is a cheat.
 
+var sortedClass = ac32folks.sort{ (a, b) -> Bool in
+    return a.1 > b.1
+}
+print(sortedClass)
 
 
-//11. Create a closure that takes an two arrays of strings as input. Output a new string with the contents of the arrays in alternating order and separated by a space. If one array's length is longer than the other, append the rest of it's contents to the new string.
 
-// eg: input array1: ["Hello", "My", "Friend"] array2: ["Darkness", "Old"]
-//      output string: "Hello Darkness My Old Friend
+//11. Create a closure that takes any two arrays of strings as input. Output a new string with the contents of the arrays in alternating order and separated by a space. If one array's length is longer than the other, append the rest of it's contents to the new string.
+
+let inputArray1 = ["Hello", "My", "Friend"]
+let inputArray2 = ["Darkness", "Old"]
+// output string: "Hello Darkness My Old Friend"
+
+
+var combArr = {(ray1: [String], ray2: [String]) -> String in
+    var outputString = ""
+    var shortIndex = 0
+    var longIndex = 0
+    var ray1Shorter = false
+    
+    if ray1.count < ray2.count {
+        shortIndex = ray1.count
+        longIndex = ray2.count
+        ray1Shorter = true
+    } else {
+        shortIndex = ray2.count
+        longIndex = ray1.count
+    }
+    
+    for i in 0..<shortIndex {
+        outputString.appendContentsOf(ray1[i] + " " + ray2[i] + " ")
+    }
+    
+    for i in shortIndex..<longIndex {
+        if ray1Shorter {
+            outputString.appendContentsOf(ray2[i] + " ")
+        } else {
+            outputString.appendContentsOf(ray1[i] + " ")
+        }
+    }
+    return outputString
+}
+print(combArr(inputArray1, inputArray2))
--- lessons/functions-two/exercises/functions-two-exercises.playground/Contents.swift
+++ lessons/functions-two/exercises/functions-two-exercises.playground/Contents.swift
@@ -5,7 +5,7 @@ import UIKit
 //1a) Write a function so that it will print out total cost after tip.
 
 let itemCost = 45
-let tipPercentage = 0.15
+let tipPercentage = 0.1
 
 //Write your code below
 
@@ -200,7 +200,7 @@ numOfUniques(inputArray)
 //7) Write a function that converts a binary number into decimal.  The binary number will be given as an array of Ints.
 
 //Sample input:
-let binaryArray = [1,0,1,1,1,0,0]
+let binaryArray = [1,0,1,1,1,0,1]
 
 //Sample output:
 //93
@@ -220,27 +220,155 @@ func binConvert(binArr: [Int]) -> Int {
 }
 binConvert(binaryArray)
 
-////8) Copy the movie dictionary from homework two and add another [String] to each movie.
-////   Name it "locations" and add some dummy data. Use the re-factored nested function
-////   from the full lesson (we didn't go over it) to print both the cast list and the 
-////   locations.
-//
-////Write your code below
-//
-//
-////9) Given one movie write a function that takes the movie (remember to use its type)
-////   as its only argument and return a tuple that contains name, year and genre.
-//
-////Write your code below
-//
-//
-////10) Define a function that takes the movie array that will call the function you just created 
-////    in answer to the previous question. The new function should return an array of the
-////    tuples: (name, year, genre).
-//
-////Write your code below
-//
-//
+//8) Copy the movie dictionary from homework two and add another [String] to each movie.
+//   Name it "locations" and add some dummy data. Use the re-factored nested function
+//   from the full lesson (we didn't go over it) to print both the cast list and the 
+//   locations.
+
+//Write your code below
+
+var movies: [[String:Any]] = [
+    [
+        "name": "Minions",
+        "year": 2015,
+        "genre": "animation",
+        "cast": ["Sandra Bullock", "Jon Hamm", "Michael Keaton"],
+        "locations": "All in the CPU",
+        "description": "Evolving from single-celled yellow organisms at the dawn of time, Minions live to serve, but find themselves working for a continual series of unsuccessful masters, from T. Rex to Napoleon. Without a master to grovel for, the Minions fall into a deep depression. But one minion, Kevin, has a plan."
+    ],
+    [
+        "name": "Shrek",
+        "year": 2001,
+        "genre": "animation",
+        "cast": ["Mike Myers", "Eddie Murphy"],
+        "locations": "All in the CPU",
+        "description": "Once upon a time, in a far away swamp, there lived an ogre named Shrek whose precious solitude is suddenly shattered by an invasion of annoying fairy tale characters. They were all banished from their kingdom by the evil Lord Farquaad. Determined to save their home -- not to mention his -- Shrek cuts a deal with Farquaad and sets out to rescue Princess Fiona to be Farquaad\"s bride. Rescuing the Princess may be small compared to her deep, dark secret."
+    ],
+    [
+        "name": "Zootopia",
+        "year": 2016,
+        "genre": "animation",
+        "cast": ["Ginnifer Goodwin", "Jason Bateman", "Idris Elba"],
+        "locations": "All in the CPU",
+        "description": "From the largest elephant to the smallest shrew, the city of Zootopia is a mammal metropolis where various animals live and thrive. When Judy Hopps becomes the first rabbit to join the police force, she quickly learns how tough it is to enforce the law."
+    ],
+    [
+        "name": "Avatar",
+        "year": 2009,
+        "genre": "action",
+        "cast": ["Sam Worthington"],
+        "locations": "Space",
+        "description": "On the lush alien world of Pandora live the Na\"vi, beings who appear primitive but are highly evolved. Because the planet\"s environment is poisonous, human/Na\"vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora. Jake Sully, a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na\"vi woman. As a bond with her grows, he is drawn into a battle for the survival of her world."
+    ],
+    [
+        "name": "The Dark Knight",
+        "year": 2008,
+        "genre": "action",
+        "cast": ["Christian Bale", "Heath Ledger", "Aaron Eckhart", "Garry Oldman", "Michael Cain", "Maggie Gyllenhal"],
+        "locations": "Gotham and London",
+        "description": "With the help of allies Lt. Jim Gordon and DA Harvey Dent, Batman has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker suddenly throws the town into chaos, the caped Crusader begins to tread a fine line between heroism and vigilantism."
+    ],
+    [
+        "name": "Transformers",
+        "year": 2007,
+        "genre": "action",
+        "cast": ["Shia LaBeouf", "Megan Fox", "Josh Duhamel"],
+        "locations": "Nevada and other places in the USA",
+        "description": "The fate of humanity is at stake when two races of robots, the good Autobots and the villainous Decepticons, bring their war to Earth. The robots have the ability to change into different mechanical objects as they seek the key to ultimate power. Only a human youth, Sam Witwicky can save the world from total destruction."
+    ],
+    [
+        "name": "Titanic",
+        "year": 1997,
+        "genre": "drama",
+        "cast": ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"],
+        "locations": "Ireland, USA and on a boat",
+        "description": "The ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built. She was the most luxurious liner of her era -- the \"ship of dreams\" -- which ultimately carried over 1,500 people to their death in the ice cold waters of the North Atlantic in the early hours of April 15, 1912."
+    ],
+    [
+        "name": "The Hunger Games",
+        "year": 2012,
+        "genre": "drama",
+        "cast": ["Jennifer Lawrence", "Josh Hutcherson", "Liam Hemsworth"],
+        "locations": "The future",
+        "description": "Katniss Everdeen voluntarily takes her younger sister\"s place in the Hunger Games, a televised competition in which two teenagers from each of the twelve Districts of Panem are chosen at random to fight to the death."
+    ],
+    [
+        "name": "American Sniper",
+        "year": 2014,
+        "genre": "drama",
+        "cast": ["Bradley Cooper", "Sienna Miller", "Kyle Gallner"],
+        "locations": "The Middle East",
+        "description": "Navy S.E.A.L. sniper Chris Kyle\"s pinpoint accuracy saves countless lives on the battlefield and turns him into a legend. Back home to his wife and kids after four tours of duty, however, Chris finds that it is the war he can\"t leave behind."
+    ]
+]
+
+
+func generateMovieReport(movieArray:[[String:Any]]?) -> String? {
+    guard let movieArray = movieArray else {
+        return nil
+    }
+    var output: String = ""
+    for movie in movieArray {
+        guard let name = movie["name"] as? String, cast = movie["cast"] as? [String], locations = movie["locations"] as? String else {
+            continue
+        }
+        var castString = ""
+        for (i, actor) in cast.enumerate() {
+            if i == cast.count - 1 {
+                castString += "and \(actor)"
+            }
+            else {
+                castString += "\(actor), "
+            }
+        }
+        output += "\(name) stars \(castString) and was filmed in \(locations)."
+        output += "\n"
+    }
+    return output
+}
+
+print(generateMovieReport(movies)!)
+
+
+
+//9) Given one movie write a function that takes the movie (remember to use its type)
+//   as its only argument and return a tuple that contains name, year and genre.
+
+//Write your code below
+
+func movieTuple(movie: [String:Any]) -> (filmName: String, filmYear: Int, filmGenre: String) {
+    guard let name = movie["name"] as? String, year = movie["year"] as? Int, genre = movie["genre"] as? String else {
+        return ("Error", -1, "Error")
+    }
+    return (name, year, genre)
+}
+
+movieTuple(movies[0])
+
+
+
+
+//10) Define a function that takes the movie array that will call the function you just created
+//    in answer to the previous question. The new function should return an array of the
+//    tuples: (name, year, genre).
+
+//Write your code below
+
+func outputArrOfTuples(movieArr: [[String:Any]]) -> [(String, Int, String)] {
+    var arrayOfTuples: [(String, Int, String)] = []
+    
+    for i in 0..<movieArr.count {
+        arrayOfTuples.append(movieTuple(movieArr[i]))
+    }
+    return arrayOfTuples
+}
+
+outputArrOfTuples(movies)
+
+
+
+
+
 ////11) Write a function that takes the movie as its only argument that lists all cast members across movies.
 ////    The tricky part: no duplicates. I think you'll need to edit the data to create some doubled actors in order
 ////    to test this. Return should be [String]?.
