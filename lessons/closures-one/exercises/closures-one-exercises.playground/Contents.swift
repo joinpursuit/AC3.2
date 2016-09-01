1881dcdc0a65746b1914a3256b9d488f54b9623d
Karen Manzanares Fuentes <Karen@Karens-MacBook-Pro-2.local>
2016-09-01 11:42:45 -0400

    exercises closures

--- lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
+++ lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
@@ -89,17 +89,16 @@ print ( closureSix ( [34,2,421,32,200], 2) )
 //5c.  Rewrite 5b and add handling for cases where x >= the count of the array (Hint: Use optionals)
 
 //Higher order functions
-
-//var closureSeven = {(a:[Int], x: Int?) -> Int in
-//    var myArray = a.sort{$0 > $1}
-//    if x > myArray.count{
-//        return nil
-//    }else {
-//        
-//    }return myArray[x-1]
-//    
-//}
-//print ( closureSix ( [34,2,421,32,200], 2) )
+var closureSeven = {(a:[Int], x: Int) -> Int? in
+    var myArray = a.sort{$0 > $1}
+    if x > myArray.count{
+        return nil
+    }else {
+        
+    return myArray[x-1]
+    }
+}
+print ( closureSeven ( [34,2,421,32,200], 7) )
 
 
 
@@ -112,17 +111,33 @@ let myArray = [34,42,42,1,3,4,3,2,49]
 
 //6a. Sort myArray in ascending order by defining the constant ascendingOrder below.
 
+
 //let mySortedArray = myArray.sort(ascendingOrder)
 //let ascendingOrder =
 
-//6b. Sort myArray in descending order by defining the constant descendingOrder below.
+
+let mySortedArray = myArray.sort({$0<$1})
+
+
+
+//6b. Sort myArray in des,cending order by defining the constant descendingOrder below.
 
 //let mySecondSortedArray = myArray.sort(descendingOrder)
 //let descendingOrder =
 
+let descendingOrfer = myArray.sort({$0>$1})
+
+
 let arrayOfArrays = [[3,65,2,4],[25,3,1,6],[245,2,3,5,74]]
 
 //7a. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Assume each array will have at least 3 elements
+
+let sortedArrayOfArrays = arrayOfArrays.sort { (a:[Int], b:[Int]) -> Bool in
+                        a[2] < b[2]
+}
+
+
+
 
 //7b. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Don't assume each array will have at least 3 elements.  Put all arrays that have less than 3 elements at the end in any order.
 
@@ -238,14 +253,57 @@ let firstAndLastTuples = [("Johann S.", "Bach"),
 //Victor Zhong	3236
 
 // Here's an example of how to start:
-let ac32folks = [("Amber", "Spadafora",	3201),
-                 ("Ana", "Ma",	3202),
-                 ("Annie", "Tung",	3203)]
+//let ac32folks = [("Amber", "Spadafora",	3201),
+//                 ("Ana", "Ma",	3202),
+//                 ("Annie", "Tung",	3203)]
 // and so on...
 
 // Build a sort comparison closure that will bring your name as close to the top as possible.
 // We will use this to determine the order we use to access the microwave.
 // Feel free to add fields to the tuple to accomplish this -- yes, this is a cheat.
+
+
+let ac32folks = [("Amber", "Spadafora",	3201),
+                 ("Ana", "Ma",	3202),
+                 ("Annie", "Tung",	3203),
+                 ("Cristopher", "Chavez", 3204),
+                 ("Eashir", "Arafat", 3205),
+                 ("Edward", "Anchundia", 3206),
+                 ("Emily", "Chu", 3207),
+                 ("Eric", "Chang", 3208),
+                 ("Erica", "Stevens", 3209),
+                 ("Fernando", "Ventura", 3210),
+                 ("Harichandan", "Singh", 3211),
+                 ("Ilmira", "Estil", 3212),
+                 ("Jermaine", "Kelly", 3213),
+                 ("Gabriel", "Breshears", 3214),
+                 ("Kadell", "Gregory", 3215),
+                 ("Kareem", "James", 3216),
+                 ("Karen", "Fuentes", 3217),
+                 ("Leandro", "Nunez", 3218),
+                 ("Liam", "Kane", 3219),
+                 ("Luz", "Herrera", 3220),
+                 ("Madushani", "Liyanage", 3221),
+                 ("Marcel", "Chaucer", 3222),
+                 ("Margaret", "Ikeda", 3223),
+                 ("Maria", "Scutaru", 3224),
+                 ("Marty", "Avedon", 3225),
+                 ("Michael", "Pinnock", 3226),
+                 ("Miti", "Shah", 3227),
+                 ("Rukiye", "Karadeniz", 3228),
+                 ("Sabrina", "Ip", 3229),
+                 ("Simone", "Grant", 3230),
+                 ("Sophia", "Barrett", 3231),
+                 ("Thinley", "Dorjee", 3232),
+                 ("Tom", "Seymour", 3233),
+                 ("Tong", "Lin", 3234),
+                 ("Tyler", "Newton", 3235),
+                 ("Victor", "Zhong", 3236)]
+
+var microwaveLine = ac32folks.sort{$0.1 > $1.1}
+for peep in microwaveLine {
+    print("\(peep.0)")
+}
 
 
 
