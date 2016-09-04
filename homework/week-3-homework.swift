22bb46beba27d8a46a2e064be809a76b3d9f21c4
Eashir Arafat <eashirarafat@C4Qs-MacBook-Pro-7.local>
2016-09-04 02:50:30 -0400

    week-3-homework.swift

---
+++ homework/week-3-homework.swift
@@ -0,0 +1,68 @@
+
+//  main.swift
+//  week-3-homework
+//
+//  Created by Eashir Arafat on 9/3/16.
+//  Copyright © 2016 Eashir Arafat. All rights reserved.
+//
+
+import Foundation
+
+
+enum Operation: String {
+    case Add = "+"
+    case Subtract = "-"
+    case Multiply = "*"
+    case Divide = "/"
+}
+
+
+func mathStuffFactory(Operation: String) -> (Double, Double) -> Double {
+    
+    switch Operation {
+    case "+":
+        return {x, y in x + y }
+    case "-":
+        return {x, y in x - y }
+    case "*":
+        return {x, y in x * y }
+    case "/":
+        return {x, y in x / y }
+    default:
+        return {x, y in x + y }
+    }
+}
+
+
+while true {
+    
+    if let response = readLine(stripNewline: true) {
+        
+        let responseString = String(response)
+        let responseSeparated = responseString.componentsSeparatedByString(" ")
+        let operation = responseSeparated[1]
+        
+        
+        
+        if operation == "?" {
+            
+            let operations = ["+","-","*","/"]
+            let randomOperation = Int(arc4random_uniform(UInt32(operations.count)))
+            let operation = String(operations[randomOperation])
+            
+            if let firstNumber = Double(responseSeparated[0]), secondNumber = Double(responseSeparated[2]) {
+                let runFunc = mathStuffFactory(operation)
+                print(runFunc(firstNumber, secondNumber))
+            }
+        }
+        
+        else {
+            if let firstNumber = Double(responseSeparated[0]), secondNumber = Double(responseSeparated[2]) {
+                let runFunc = mathStuffFactory(operation)
+                print(runFunc(firstNumber, secondNumber))
+            }
+        }
+        
+    }
+    
+}
