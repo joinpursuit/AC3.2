46472f3d0c8ab102cf99cab5a1b32d5dd6c8d84b
harichandansingh <singh.harichandan@gmail.com>
2016-09-05 17:01:27 -0400

    Week 3 HW 1.2

--- homework/week-3/week3-hw/week3-hw/main.swift
+++ homework/week-3/week3-hw/week3-hw/main.swift
@@ -6,6 +6,22 @@ import Foundation
 
 //      1B -  1D
 
+enum Operator:String {
+    case Plus = "+"
+    case Minus = "-"
+    case Times = "*"
+    case DividedBy = "/"
+    case Random = "?"
+}
+
+
+enum Command:String {
+    case LessThan6 = "filter"
+    case Squared = "map"
+    case Math = "math"
+}
+
+
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     switch opString {
     case "+":
@@ -22,160 +38,103 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
 }
 
 
-enum Operator:String {
-    case Plus = "+"
-    case Minus = "-"
-    case Times = "*"
-    case DividedBy = "/"
-    case Random = "?"
-}
-
-
-while true {
-    print("Let's do some simple math. Enter two operands and an operator: ", terminator: "")
-    if let response = readLine(stripNewline: true) {
-        if response == "exit" {
-            break
-        }
-        
-        
-        let commands = response.componentsSeparatedByString(" ")
-        let operatorSymbol = commands[1]
-        let operatorConversion = Operator(rawValue : operatorSymbol)
-        
-        
-        if (operatorSymbol != "+" && operatorSymbol != "-" && operatorSymbol != "*" && operatorSymbol != "/" && operatorSymbol != "?") {
-            print("Unknown operator: \(operatorSymbol)")
-            continue
-        }
-        else {
-        if let operand1 = Double(commands[0]), operand2 = Double(commands[2]), x = operatorConversion {
-            
-            
-            var operation = mathStuffFactory(operatorSymbol)
-            var answer = operation(operand1, operand2)
-            
-            var operatorArray = ["+","-","*","/"]
-            var index = UInt32(operatorArray.count - 1)
-            
-            var randomOperatorSymbol = operatorArray[Int(arc4random_uniform(index))]
-            var randomOperation = mathStuffFactory(randomOperatorSymbol)
-            var randomAnswer = randomOperation(operand1, operand2)
-            
-            
-                switch x {
-                case .Plus, .Minus, .Times, .DividedBy:
-                    print("\(operand1) \(operatorSymbol) \(operand2) = \(answer). Try another operation!")
-                case .Random:
-                    while true {
-                    print(" \(randomAnswer). Guess the operator used!")
-                    if let randomPrompt = readLine(stripNewline: true) {
-                        if randomPrompt == randomOperatorSymbol {
-                            print("Correct!")
-                            break
-                        }
-                        else {
-                            print("Nope!")
-                        }
-                        }
-                    }
-            }
-            }
-        }
+func myMap(inputArray: [Int] = [4,2,6,3,4,6,2,8,5], filter: (Int) -> Int) -> [Int] {
+    var newArray = [Int]()
+    for i in inputArray {
+        newArray.append(filter(i))
     }
+    print(newArray)
+    return newArray
 }
 
 
-//      1A
-
-//while true {
-//    print("Let's do some simple math. Enter two operands and an operator: ", terminator: "")
-//    if let response = readLine(stripNewline: true) {
-//        if response == "exit" {
-//            break
-//        }
-//        
-//        let commands = response.componentsSeparatedByString(" ")
-//        let operatorSymbol = commands[1]
-//        
-//        if (operatorSymbol == "+" || operatorSymbol == "-" || operatorSymbol == "*" || operatorSymbol == "/") {
-//            let operation = mathStuffFactory(commands[1])
-//            if let operand1 = Double(commands[0]), operand2 = Double(commands[2]) {
-//                let answer = operation(operand1, operand2)
-//                print("\(operand1) \(operatorSymbol) \(operand2) = \(answer). Try another operation!")
-//                continue
-//            }
-//        }
-//        else {
-//            print("Unknown operator: \(operatorSymbol)")
-//            continue
-//        }
-//    }
-//}
-
-
-//      2
-
-let ints = [4,2,6,3,4,6,2,8,5]
-
-
-func myFilter(inputArray: [Int], filter: (Int) -> Bool) -> [Int] {
-    
+func myFilter(inputArray: [Int] = [4,2,6,3,4,6,2,8,5], filter: (Int) -> Bool) -> [Int] {
     var newArray = [Int]()
     for i in inputArray {
         if filter(i) {
             newArray.append(i)
         }
     }
+    print(newArray)
     return newArray
 }
 
 
-var evenFiltered = myFilter(ints) { (x: Int) -> Bool in
-    x % 2 == 0
-}
-print(evenFiltered)
-
-
-var oddFiltered = myFilter(ints) { (x: Int) -> Bool in
-    x % 2 == 1
-}
-print(oddFiltered)
-
-
-var greaterThanEqualTo6 = myFilter(ints) { (x: Int) -> Bool in
-    x >= 6
-}
-print(greaterThanEqualTo6)
-
-
-//      3
-
-let intsB = [4,2,6,3,4,6,2,8,5]
-
-func myMap(inputArray: [Int], filter: (Int) -> Int) -> [Int] {
-
-    var newArray = [Int]()
-    for i in inputArray {
-        newArray.append(filter(i))
+func processCommand(arr:[String]) {
+    if let command = Command(rawValue: arr[0]) {
+        switch command {
+        case .Math:
+            while true {
+            print("Enter two operands and an operator", terminator: "")
+            if let mathResponse = readLine(stripNewline: true) {
+                    if mathResponse == "exit" {
+                        break
+                    }
+            let mathCommands = mathResponse.componentsSeparatedByString(" ")
+            let operatorSymbol = mathCommands[1]
+            let operatorConversion = Operator(rawValue : operatorSymbol)
+                if (operatorSymbol != "+" && operatorSymbol != "-" && operatorSymbol != "*" && operatorSymbol != "/" && operatorSymbol != "?") {
+                    print("Unknown operator: \(operatorSymbol)")
+                }
+                else {
+                    if let operand1 = Double(mathCommands[0]), operand2 = Double(mathCommands[2]), x = operatorConversion {
+                        
+                        
+                        let operation = mathStuffFactory(operatorSymbol)
+                        let answer = operation(operand1, operand2)
+                        
+                        let operatorArray = ["+","-","*","/"]
+                        let index = UInt32(operatorArray.count - 1)
+                        
+                        let randomOperatorSymbol = operatorArray[Int(arc4random_uniform(index))]
+                        let randomOperation = mathStuffFactory(randomOperatorSymbol)
+                        let randomAnswer = randomOperation(operand1, operand2)
+                        
+                        
+                        switch x {
+                        case .Plus, .Minus, .Times, .DividedBy:
+                            print("\(operand1) \(operatorSymbol) \(operand2) = \(answer). Try another operation!")
+                        case .Random:
+                            while true {
+                                print(" \(randomAnswer). Guess the operator used!")
+                                if let randomPrompt = readLine(stripNewline: true) {
+                                    if randomPrompt == randomOperatorSymbol {
+                                        print("Correct!")
+                                        break
+                                    }
+                                    else {
+                                        print("Nope!")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                }
+            }
+        case .LessThan6:
+            myFilter {(x: Int) -> Bool in
+                x <= 6
+            }
+        case .Squared:
+            myMap {(x: Int) -> Int in
+                return x * x
+            }
+        }
     }
-    return newArray
+    
 }
 
 
-var plusTen = myMap(intsB) { (x: (Int)) -> Int in
-    x + 10
+while true {
+    print("Type a command to get started: ", terminator: "")
+    if let response = readLine(stripNewline: true) {
+        if response == "exit" {
+            break
+        }
+        
+        let commands = response.componentsSeparatedByString(" ")
+        
+        processCommand(commands)
+    }
 }
-print(plusTen)
-
-
-var timesTen = myMap(intsB) {(x: Int) -> Int in
-    x * 10
-}
-print(timesTen)
-
-
-var squared = myMap(intsB) {(x: Int) -> Int in
-    x * x
-}
-print(squared)
