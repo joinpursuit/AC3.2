ebb977c55befd92a7f2dc9e6a12c37081c0eba96
Simone Grant <simonegrant@C4Qs-MacBook-Pro-96.local>
2016-09-06 12:36:18 -0400

    Homework Wk3

--- lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
+++ lessons/closures-one/exercises/closures-one-exercises.playground/Contents.swift
@@ -119,61 +119,10 @@ let descendingOrder = myArray.sort({
 var arrayOfArrays = [[3,65,2,4],[25,3,1,6],[245,2,3,5,74]]
 
 //7a. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Assume each array will have at least 3 elements
-
-//sort by 
 //
-//var arraySort: ([Int]) -> ([Int]) = { x -> ([Int]) in
-//for arrays in arrayOfArrays {
-//    for i in arrays {
-//        if i == arrays[2] {
-//            var mutArray = arrays[i]
-//            var arraySort: [Int] = arrayOfArrays.sort({ $0.mutArray < $1.mutArray })
-//            return arraySort
-//        }
-//    }
-//    }
-
-//var arraySort: Int -> Int = { array -> Int in
-//for arrays in arrayOfArrays {
-//    var sortNum: Int
-//    for i in arrays {
-//        if i == arrays[2] {
-//            //return arrays[i]
-//            let sortNum = arrays[i]
-//            return arrayOfArrays.sortInPlace({ $0.arrayOfArrays[sortNum] < $1.arrayOfArrays[sortNum] })
-//            }
-//       
-//        }
-//    
-//    }
-//    
-//}
-//
-//var arraySort: Int -> Int = { array -> Int in
-//    for arrays in arrayOfArrays {
-//        var sortNum: Int
-//        
-//        for i in arrays {
-//            if i == arrays[2] {
-//                return arrays[i]
-//            }
-//            var sortNum = arrays[i]
-//            return sortNum
-//        }
-//    }
-//    return  $0 < $1
-//}
-
-
-/*
-var myNum: ([Int], Int) -> Int? = { numbers, x -> Int? in
-    var newNum = numbers
-    newNum.sortInPlace(<)//sorts ascending
-    if x > newNum.count {
-        return nil
-    }
-    return newNum[x]
-*/
+//arrayOfArrays.sort({ (a: Int, b: Int) -> Int in
+//    return a < b
+//})
 
 //7b. Sort arrayOfArrays in ascending order by the 3rd element in each array.  Don't assume each array will have at least 3 elements.  Put all arrays that have less than 3 elements at the end in any order.
 
@@ -210,8 +159,11 @@ let letterValues = [
 
 var codeString = "aldfjaekwjnfaekjnf"
 
+
+
 let myString = Array(codeString.characters)
 
+myString[4]
 myString.sort({ (a: Character, b: Character) -> Bool in
         let aValue = letterValues[String(a)]
         let bValue = letterValues[String(b)]
@@ -296,14 +248,53 @@ let firstAndLastTuples = [("Johann S.", "Bach"),
 //Victor Zhong	3236
 
 // Here's an example of how to start:
-let ac32folks = [("Amber", "Spadafora",	3201),
-                 ("Ana", "Ma",	3202),
-                 ("Annie", "Tung",	3203)]
+//let ac32folks = [("Amber", "Spadafora",	3201),
+//                 ("Ana", "Ma",	3202),
+//                 ("Annie", "Tung",	3203)]
 // and so on...
 
 // Build a sort comparison closure that will bring your name as close to the top as possible.
 // We will use this to determine the order we use to access the microwave.
 // Feel free to add fields to the tuple to accomplish this -- yes, this is a cheat.
+
+let ac32folks: [(String, String, Int)] = [ ("Amber", "Spadafora", 3201),
+("Ana", "Ma", 3202),
+("Annie", "Tung", 3203),
+("Cristopher", "Chavez", 3204),
+("Eashir", "Arafat", 3205),
+("Edward", "Anchundia", 3206),
+("Emily", "Chu", 3207),
+("Eric", "Chang", 3208),
+("Erica", "Stevens", 3209),
+("Fernando", "Ventura", 3210),
+("Harichandan", "Singh", 3211),
+("Ilmira", "Estil",	3212),
+("Jermaine", "Kelly", 3213),
+("Gabriel", "Breshears", 3214),
+("Kadell", "Gregory", 3215),
+("Kareem", "James",	3216),
+("Karen", "Manzanares Fuentes", 3217),
+("Leandro", "Nunez", 3218),
+("Liam", "Kane", 3219),
+("Luz", "Loayza Herrera", 3220),
+("Madushani", "Lekam Wasam Liyanage", 3221),
+("Marcel", "Chaucer", 3222),
+("Margaret", "Ikeda", 3223),
+("Maria", "Scutaru",	3224),
+("Marty", "Avedon",	3225),
+("Michael", "Pinnock", 3226),
+("Miti", "Shah", 3227),
+("Rukiye", "Karadeniz",	3228),
+("Sabrina", "Ip", 3229),
+("Simone", "Grant",	3230),
+("Sophia", "Barrett", 3231),
+("Thinley", "Dorjee", 3232),
+("Tom", "Seymour", 3233),
+("Tong", "Lin", 3234),
+("Tyler", "Newton",	3235),
+("Victor", "Zhong",	3236)
+]
+
 
 
 
