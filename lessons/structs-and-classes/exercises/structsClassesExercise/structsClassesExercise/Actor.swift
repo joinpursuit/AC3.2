bb56ff327f26525f8b0930363c1d3194cd2ddd91
Victor Zhong <viczhong@gmail.com>
2016-09-06 17:56:43 -0400

    9-06 Exercise

---
+++ lessons/structs-and-classes/exercises/structsClassesExercise/structsClassesExercise/Actor.swift
@@ -0,0 +1,14 @@
+//
+//  Actor.swift
+//  structsClassesExercise
+//
+//  Created by Victor Zhong on 9/6/16.
+//  Copyright © 2016 Victor Zhong. All rights reserved.
+//
+
+import Foundation
+
+class Actor: Person {
+	var breakoutYear = 0
+	var breakoutRole = ""
+}
---
+++ lessons/structs-and-classes/exercises/structsClassesExercise/structsClassesExercise/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  structsClassesExercise
+//
+//  Created by Victor Zhong on 9/6/16.
+//  Copyright © 2016 Victor Zhong. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+	var name = String()
+	var yearBorn = 0
+	var yearDied = 0
+}
