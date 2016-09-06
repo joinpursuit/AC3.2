daac1509dfc66ca55f66d47ea7ae740da8c60263
Erica Y Stevens <ericastevens99@gmail.com>
2016-09-06 14:15:11 -0400

    commits

---
+++ lessons/structs-and-classes/exercises/structsClassesExercises/structsClassesExercises/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  structsClassesExercises
+//
+//  Created by Erica Y Stevens on 9/6/16.
+//  Copyright © 2016 Erica Stevens. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    var name = String()
+    var year = Int()
+    var genre = String()
+    var cast = [String]()
+    var description = String()
+}
+
